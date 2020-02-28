e30c9a21d92d37c2bd5eabfacd4983e0fc3c637d
Matt Colson <mc180g@gmail.com>
2020-02-27 21:33:39 -0500

    removed decimal from hours

--- countdown.js
+++ countdown.js
@@ -4,6 +4,6 @@ var __startTime = moment().format();
 var __endTime = moment("2025-03-17T06:00").format();
 
 var __duration = moment.duration(moment(__endTime).diff(__startTime));
-var __hours = __duration.asHours();
+var __hours = __duration.asHours().toFixed(0);
 console.log(__hours);
 launch.textContent = __hours;
