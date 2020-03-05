8ad79188c57f2cef5bbef26b73bd301267a8da9d
Matt Colson <mc180g@gmail.com>
2020-03-05 01:18:16 -0500

    changed launch date

--- countdown.js
+++ countdown.js
@@ -1,7 +1,7 @@
 const launch = document.getElementById('launch');
 
 var __startTime = moment().format();
-var __endTime = moment("2025-03-17T06:00").format();
+var __endTime = moment("2020-07-17T06:00").format();
 
 var __duration = moment.duration(moment(__endTime).diff(__startTime));
 var __hours = __duration.asHours().toFixed(0);
