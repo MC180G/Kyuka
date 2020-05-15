dbdd08195ce5a77b27a0a3b2dce20cab46539f22
Matt Colson <mc180g@gmail.com>
2020-05-14 21:18:42 -0400

    changed background opacity

--- clock.js
+++ clock.js
@@ -1,5 +1,3 @@
-//The clock waits 60000ms to load the time.
-
 const clock = document.getElementById('clock');
 
 setInterval(function tick() {
@@ -8,5 +6,3 @@ setInterval(function tick() {
     clock.textContent = clockFace;
     return tick;
 }(), 60000);
-
-//Make a seperate function in order to load the clock first then call the clock every 60000ms.
